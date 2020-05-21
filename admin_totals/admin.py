70cf325f9985b46404e81803f34443680b4fc6c5
Tuk Bredsdorff <tukodeb@gmail.com>
2020-05-21 16:27:27 +0200

    Added support for computed properties

--- admin_totals/admin.py
+++ admin_totals/admin.py
@@ -13,8 +13,14 @@ class ChangeListTotals(ChangeList):
             list_totals = dict(self.model_admin.list_totals)
             for field in self.list_display:
                 if field in list_totals:
-                    self.aggregations.append(
-                        self.result_list.aggregate(agg=list_totals[field](field))['agg'])
+                    if type(list_totals[field]) != tuple:
+                        model_field = field
+                        func = list_totals[field]
+                        formatter = str
+                    else:
+                        func, model_field, formatter = list_totals[field]
+                    total = formatter(self.result_list.aggregate(agg=func(model_field))['agg'])
+                    self.aggregations.append(total)
                 else:
                     self.aggregations.append('')
 
